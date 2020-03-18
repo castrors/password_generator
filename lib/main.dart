936b4e3441229523faf699afe9ac45ce3d82bdc1
Rodrigo Castro <rodrigodesouzacastro@gmail.com>
2020-03-17 21:06:54 -0300

    Draw the components

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(MyApp());
+final _fieldColors = Color(0xFF0b1740);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+void main() => runApp(App());
+
+class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+          brightness: Brightness.dark,
+          accentColor: Color(0xFF357ced),
+          primaryColor: Color(0xFF357ced),
+          scaffoldBackgroundColor: Color(0xFF060f2d),
+          fontFamily: GoogleFonts.nunito().fontFamily),
+      home: PasswordGeneratorPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class PasswordGeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text(
-              'You have pushed the button this many times:',
+              'Generate password',
             ),
             Text(
-              '$_counter',
+              'Generated password',
+            ),
+            Text(
+              'cDSVKctQx71hCKnu',
               style: Theme.of(context).textTheme.display1,
             ),
+            Text(
+              'LENGTH: 16',
+            ),
+            Slider(
+              onChanged: (value) {},
+              value: 16,
+              max: 32,
+              min: 4,
+            ),
+            Text('SETTINGS'),
+            SwitchListTile(
+              onChanged: (value) {},
+              title: Text('Include uppercase letters'),
+              value: true,
+            ),
+            SwitchListTile(
+              onChanged: (value) {},
+              title: Text('Include lowercase letters'),
+              value: true,
+            ),
+            SwitchListTile(
+              onChanged: (value) {},
+              title: Text('Include numbers'),
+              value: true,
+            ),
+            SwitchListTile(
+              onChanged: (value) {},
+              title: Text('Include symbols'),
+              value: true,
+            ),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 32.0),
+                  child: SizedBox(
+                    height: 52,
+                    width: MediaQuery.of(context).size.width,
+                    child: RaisedButton(
+                      child: Text('GENERATE PASSWORD'),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
